070705f632c73bc021f43af0d15d972982b37d8b
Jonathan Vincent <jonathan.vincent@siemens-healthineers.com>
2023-11-12 17:22:34 +0100

    update setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+   name='math_quiz',
+   version='1.0',
+   description='A math quiz game',
+   author='Jonathan Vincent',
+   author_email='jonathan.vincent@fau.de',
+   packages=['math_quiz'],  #same as name
+)
